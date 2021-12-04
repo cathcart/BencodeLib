3ee26c18b1731661ae1de166d0a5e6873c9d6e4c
cathcart <thomas.cathcart@gmail.com>
2021-12-04 22:33:24 +0000

    Updates to allow encoding of strings to bencode strings

--- BencodeLibRedo/BencodeEncoder.cs
+++ BencodeLibRedo/BencodeEncoder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using BencodeLibRedo.Interfaces;
 using BencodeLibRedo.Models;
 
@@ -9,23 +11,57 @@ public class BencodeEncoder
     {
     }
 
-    public string Encode(IBencodeItem input)
+    public string Encode(dynamic input)
     {
+        if(input is string)
+        {
+            int inputInt;
+            var isInt = Int32.TryParse(input, out inputInt);
 
-        Console.WriteLine(input.GetType());
-        //switch (input.GetType())
-        //{
-        //    case BencodeInt:
-        //        break;
-        //    case BencodeString:
-        //        break;
-        //    case BencodeDict:
-        //        break;
-        //    case BencodeList:
-        //        break;
-        //    default:
-        //        throw new InvalidDataException(string.Format("encoder doesn't recognise input type {0}", output));
-        //}
-        return "";
+            if(isInt == true)
+            {
+                return Encode(inputInt);
+            }
+            else
+            {
+                var s = string.Format("{0}:{1}", ((string)input).Length, input);
+                return s;
+            }
+       
+        }
+        else if(input is int)
+        {
+            var s = string.Format("i{0}e", (input));
+            return s;
+        }
+        else if(input.GetType() == (new List<string>()).GetType())
+        {
+            var s = "l";
+            foreach(var x in input)
+            {
+                s += Encode(x);
+            }
+            s += "e";
+            return s;
+        }
+        else if (input.GetType() == (new Dictionary<string, string>()).GetType())
+        {
+            var inputDict = (Dictionary<string, string>)input;
+            var list = inputDict.Keys.ToList<string>();
+            list.Sort();
+
+            var s = "d";
+            foreach (var x in list)
+            {
+                s += Encode(x);
+                s += Encode(inputDict[x]);
+            }
+            s += "e";
+            return s;
+        }
+        else
+        {
+            throw new Exception(string.Format("Can't encode type {0}", input.type()));
+        }
     }
 }
